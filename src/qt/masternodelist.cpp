b8a5839f894273fc3458b82ca377b816e509961d
root <root@localhost.localdomain>
2018-09-16 11:03:06 -0400

    change bg

--- src/qt/masternodelist.cpp
+++ src/qt/masternodelist.cpp
@@ -96,7 +96,7 @@ void MasternodeList::StartAlias(std::string strAlias)
             std::string strError;
             CMasternodeBroadcast mnb;
 
-            bool fSuccess = CMasternodeBroadcast::Create(mne.getIp(), mne.getPrivKey(), mne.getTxHash(), mne.getOutputIndex(), strError, mnb);
+            bool fSuccess = activeMasternode.Register(mne.getIp(), mne.getPrivKey(), mne.getTxHash(), mne.getOutputIndex(), strError);
 
             if (fSuccess) {
                 strStatusHtml += "<br>Successfully started masternode.";
@@ -136,7 +136,7 @@ void MasternodeList::StartAll(std::string strCommand)
 
         if (strCommand == "start-missing" && pmn) continue;
 
-        bool fSuccess = CMasternodeBroadcast::Create(mne.getIp(), mne.getPrivKey(), mne.getTxHash(), mne.getOutputIndex(), strError, mnb);
+        bool fSuccess = activeMasternode.Register(mne.getIp(), mne.getPrivKey(), mne.getTxHash(), mne.getOutputIndex(), strError);
 
         if (fSuccess) {
             nCountSuccessful++;
